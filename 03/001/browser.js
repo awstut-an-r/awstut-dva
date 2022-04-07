edd8c5a6c3797bb3d61a18d24174cf5f1f6b03f1
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-04-07 18:32:10 +0900

    Update browser.js

--- 03/001/browser.js
+++ 03/001/browser.js
@@ -28,7 +28,6 @@ const cognitoIdentityClient = new CognitoIdentityClient({ region: REGION });
 const cognitoSyncClient = new CognitoSyncClient({
   region: REGION,
   credentials: fromCognitoIdentityPool({
-    //client: new CognitoIdentityClient({ region: REGION }),
     client: cognitoIdentityClient,
     identityPoolId: IDENTITY_POOL_ID,
     logins: {
@@ -38,22 +37,18 @@ const cognitoSyncClient = new CognitoSyncClient({
 });
 
 const main = async () => {
-  const response = await cognitoIdentityClient.send(
-    new GetIdCommand({
-      IdentityPoolId: IDENTITY_POOL_ID,
-      Logins: {
-        [`cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}`]: idToken
-      }
-    })
-  );
-  //console.log(response);
-  
-  const identityId = response.IdentityId;
-  //console.log(identityId);
-  
-  const setBackgroundColorToCognitoSyncDataset = async (color) => {
-    //console.log(identityId);
-    try {
+  try {
+    const response = await cognitoIdentityClient.send(
+      new GetIdCommand({
+        IdentityPoolId: IDENTITY_POOL_ID,
+        Logins: {
+          [`cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}`]: idToken
+        }
+      })
+    );
+    
+    const identityId = response.IdentityId;
+    const setBackgroundColorToCognitoSyncDataset = async (color) => {
       const listRecordsResponse = await cognitoSyncClient.send(
         new ListRecordsCommand({
           DatasetName: DATASET,
@@ -61,19 +56,16 @@ const main = async () => {
           IdentityPoolId: IDENTITY_POOL_ID
         })
       );
-      //console.log(listRecordsResponse);
       const syncSessionToken = listRecordsResponse.SyncSessionToken;
       const syncCount = (() => {
         for (let record of listRecordsResponse.Records) {
-          console.log(record);
+          //console.log(record);
           if (record.Key == DATASET_KEY) {
             return record.SyncCount + 1;
           }
         }
-        
         return 0;
       })();
-      console.log(syncCount);
       
       const response = await cognitoSyncClient.send(
         new UpdateRecordsCommand({
@@ -89,44 +81,52 @@ const main = async () => {
           SyncSessionToken: syncSessionToken
         })
       );
-      //console.log(response);
       document.body.style.background = color;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  const getBackgroundColorFromCognitoSyncDataset = async () => {
-    const listRecordsResponse = await cognitoSyncClient.send(
-      new ListRecordsCommand({
-        DatasetName: DATASET,
-        IdentityId: identityId,
-        IdentityPoolId: IDENTITY_POOL_ID
-      })
-    );
-    //console.log(listRecordsResponse);
+    };
     
-    for (let record of listRecordsResponse.Records) {
-      if (record.Key == DATASET_KEY) {
-        document.body.style.background = record.Value;
-        break;
+    const getBackgroundColorFromCognitoSyncDataset = async () => {
+      const listRecordsResponse = await cognitoSyncClient.send(
+        new ListRecordsCommand({
+          DatasetName: DATASET,
+          IdentityId: identityId,
+          IdentityPoolId: IDENTITY_POOL_ID
+        })
+      );
+      
+      for (let record of listRecordsResponse.Records) {
+        if (record.Key == DATASET_KEY) {
+          document.body.style.background = record.Value;
+          break;
+        }
       }
-    }
-  };
+    };
+      
+    window.setBackgroundColorToCognitoSyncDataset = setBackgroundColorToCognitoSyncDataset;
+    window.getBackgroundColorFromCognitoSyncDataset = getBackgroundColorFromCognitoSyncDataset;
     
-  window.setBackgroundColorToCognitoSyncDataset = setBackgroundColorToCognitoSyncDataset;
-  window.getBackgroundColorFromCognitoSyncDataset = getBackgroundColorFromCognitoSyncDataset;
+    document.getElementById("button-set").addEventListener("click", function(event) {
+      var color = document.getElementById("background-color").value;
+      setBackgroundColorToCognitoSyncDataset(color);
+    });
+    
+    document.getElementById('button-get').addEventListener('click', function(event) {
+      getBackgroundColorFromCognitoSyncDataset();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  
+  // get username
+  try {
+    const tokens = idToken.split('.');
+    const tokenDecoded = JSON.parse(atob(tokens[1]));
+    document.getElementById('name').innerText = `Name: ${tokenDecoded.name}`;
+  } catch (err) {
+    console.log(err);
+    document.getElementById('name').innerText = 'Name: Guest';
+  }
 };
 
 main();
 
 
-document.getElementById("button-set").addEventListener("click", function(event) {
-  var color = document.getElementById("background-color").value;
-  //console.log(color);
-  setBackgroundColorToCognitoSyncDataset(color);
-});
-
-document.getElementById('button-get').addEventListener('click', function(event) {
-  getBackgroundColorFromCognitoSyncDataset();
-});
